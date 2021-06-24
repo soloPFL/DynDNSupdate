de3d13842d09d64cdca646276a28166470c1dcb7
soloPFL <86072748+soloPFL@users.noreply.github.com>
2021-06-24 18:11:31 +0200

    Create dyndns-update-userinput.sh

---
+++ dyndns-update-userinput.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ipinfo=$(curl ifconfig.co/ip)
+
+# https://github.com/soloPFL/DynDNSupdate
+# http://creativecommons.org/licenses/by-sa/4.0/
+# Please add your username, password and host you want to update in the next lines.
+# then just execute the script to update the ip.
+
+read -p 'Input Host to update : ' hostinput
+read -p 'Input Username : ' usernameinput
+read -p 'Input Password : ' pwinput
+user=$usernameinput
+password=$pwinput
+host=$hostinput
+echo "....."
+echo "my ip"
+echo "$ipinfo"
+
+curl https://$user:$password@dyndns.strato.com/nic/update?hostname=$host&myip=$ipinfo
+echo "$host status update complete"
